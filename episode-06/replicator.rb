7078010a9c110dcbab23f23e4dd79cc4caf2dae6
tylteng <teng.ty4@gmail.com>
2017-11-10 15:28:18 -0500

    debugged eipsode 6

--- episode-06/replicator.rb
+++ episode-06/replicator.rb
@@ -1,3 +1,4 @@
+# require 'pry'
 class Replicator
 
   # When the Enterprise calls Replicator.new, this method executes.
@@ -40,7 +41,7 @@ class Replicator
   # and then manually execute each method to ensure
   # it returns what's expect.
   def replicate(recipe)
-
+    # binding.pry
     # Setup an instance variable for the recipe
     # so that other methods can see what the recipe is
     @recipe = recipe
@@ -88,6 +89,7 @@ class Replicator
 
   # This moves the glass from the cupboard to inside the replicator.
   def retrieve_glass
+
     @enterprise.transporter.energize(
       @enterprise.cupboard.find_glass,
       @enterprise.cupboard.shelf,
